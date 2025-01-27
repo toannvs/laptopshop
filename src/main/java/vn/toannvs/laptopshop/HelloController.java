878f40d26408483ce31bb3b7278d1040dccc6349
toannvs <toannguyenvosong@gmail.com>
2025-01-27 22:55:50 +0700

    Add user and admin endpoints to HelloController
    - Added userPage method to handle "/user" endpoint
    - Added adminPage method to handle "/admin" endpoint
    - Updated HelloController to include new endpoints with appropriate messages

--- src/main/java/vn/toannvs/laptopshop/HelloController.java
+++ src/main/java/vn/toannvs/laptopshop/HelloController.java
@@ -10,5 +10,15 @@ public class HelloController {
     public String index() {
         return "Hello World! My name is Toan";
     }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page";
+    }
+    
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page";
+    }
     
 }
