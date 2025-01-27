82b743d03018617a430ee697ed7c3ec30d1b7207
Nguyễn Võ Song Toàn <toannguyenvosong@gmail.com>
2025-01-27 22:09:24 +0700

    Initialize Spring Boot project

---
+++ src/test/java/vn/toannvs/laptopshop/LaptopShopApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.toannvs.laptopshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
