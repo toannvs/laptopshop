0a94c303b7f47df2feaa1a9ec5963fe8d09db78a
toannvs <toannguyenvosong@gmail.com>
2025-02-14 00:18:27 +0700

    Add Category entity
    - Created `Category` entity to represent product categories.
    - Included fields `id`, `name`, and `description`.
    - Added JPA annotations for table mapping and primary key generation.
    - Used Lombok's @Data annotation to generate boilerplate code for getters, setters, and other utility methods.

---
+++ src/main/java/vn/toannvs/laptopshop/entity/Category.java
@@ -0,0 +1,18 @@
+package vn.toannvs.laptopshop.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+}
